05d9ec762567570bcc49f791b70896e038f36c4f
vinvas94 <126397140+vinvas94@users.noreply.github.com>
2023-10-31 14:39:41 +0100

    add task 1 y 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+listEl.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(element) {
+  return element
+    .map(
+      ({ preview, original, description }) => `
+
+  <li class="gallery__item">
+   <a class="gallery-link" href="${original}">
+ <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    /></a>
+ </li>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

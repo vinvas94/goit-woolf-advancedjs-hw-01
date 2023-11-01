d6a970485ca1fe34d84515085352450f3875841b
vinvas94 <vintoniv1111@gmail.com>
2023-11-01 16:15:28 +0100

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+const localStorageKey = 'feedback-form-state';
+
+let formData = {};
+
+form.addEventListener('input', throttle(handlerForm, 500));
+
+function handlerForm(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', handlerBut);
+
+function handlerBut(evt) {
+  evt.preventDefault();
+  const formElement = evt.target.elements;
+  const email = formElement.email.value;
+  const message = formElement.message.value;
+  const data = {
+    email,
+    message,
+  };
+  if (email === '' || message === '') {
+    return alert('Please fill in all the fields!');
+  }
+  formData = {};
+  evt.currentTarget.reset();
+  console.log(data);
+  localStorage.removeItem(localStorageKey);
+}
+
+function loadFormData() {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    for (let key in formData) {
+      form[key].value = formData[key];
+    }
+  }
+}
+loadFormData();
